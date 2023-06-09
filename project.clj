ab860489a49df54f6ad9af61c8a7aad024fcbf7e
Circle CI <circleci@logicblocks.io>
2023-06-09 11:16:03 +0000

    Pre-release version 6.0.1-RC1 [skip ci]

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.logicblocks/halboy "6.0.0"
+(defproject io.logicblocks/halboy "6.0.1-RC1"
   :description "a hypermedia parser and navigator"
   :license {:name "MIT License"
             :url  "https://opensource.org/licenses/MIT"}
